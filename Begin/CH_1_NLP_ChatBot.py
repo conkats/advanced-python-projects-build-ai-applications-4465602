0659b2e47637e65e0f1fdec7d7cd9730e3dd42e5
Constantinos Katsamis <52401040+conkats@users.noreply.github.com>
2024-07-18 09:11:46 +0100

    Creating my own ChatBox
    -Using TextBlob library for sentiment language interpretation and creating a chatbox class

--- Begin/CH_1_NLP_ChatBot.py
+++ Begin/CH_1_NLP_ChatBot.py
@@ -1,4 +1,7 @@
 # Importing TextBlob to help the chatbot understand language nuances.
+# This line imports the TextBlob class from the TextBlob module, allowing you to use TextBlob's 
+# natural language processing functionalities in your Python script. TextBlob is a Python library 
+# for processing textual data. It provides a simple API for common NLP tests and sentiment analysis.
 from textblob import TextBlob
 
 
@@ -6,7 +9,10 @@ from textblob import TextBlob
 class ChatBot:
     def __init__(self):
         # Initializing the sentiment analysis tool.
-        self.sentiment_analyzer = TextBlob("")
+        #This creates an instance variable and initializes it
+        # with an empty TextBlob object. This object will serve as 
+        # our tool for sentiment analysis. Now let's define a method called chart stat. Def start_chat self. Then we'll print a grading message from the ChatBot to the console. 
+        self.sentiment_analyzer = TextBlob("") 
 
     def start_chat(self):
         print("ChatBot: Hi, how can I help you?")
@@ -15,16 +21,17 @@ class ChatBot:
 
             # Analyzing the sentiment of the user's message.
             self.sentiment_analyzer = TextBlob(user_message)
+            #computes the polarity score-emotional skill of chatbox
             sentiment_score = self.sentiment_analyzer.sentiment.polarity
 
             # Generating the chatbot's response based on sentiment.
-            if sentiment_score > 0:
-                chatbot_message = f"ChatBot: That's great to hear! \n Sentiment score: {sentiment_score}\n"
-            elif sentiment_score < 0:
-                chatbot_message = f"ChatBot: I'm sorry to hear that. \n Sentiment score: {sentiment_score}\n"
-            else:
-                chatbot_message = f"ChatBot: Hmm, I see. \n Sentiment score: {sentiment_score}\n"
-
+            if sentiment_score > 0 :#more +ve sentiment
+                chatbot_message = f"ChatBot: That's great to hear! \n Sentiment Score {sentiment_score}\n"
+            elif sentiment_score <0:#more -ve sentiment
+                chatbot_message = f"ChatBot: I am sorry to hear that. Would you like me to transfer you to a live agent? \n Sentiment Score: {sentiment_score}\n"
+            else:#neutral statement
+                chatbot_message=f"ChatBot: Hmm, I see. Can you please provide more information? \n Sentiment Score:{sentiment_score}\n"
+            
             # Printing the chatbot's response and sentiment.
             print(chatbot_message)
 
